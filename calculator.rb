c71eabf65101fb166eda538b615357bbbd24e962
odesey <odesey@gmail.com>
2013-03-27 23:30:32 -0400

    For some reason my last push didnt have any of my changes, this one has the corrected buttons and a working clear button

--- calculator.rb
+++ calculator.rb
@@ -1,14 +1,17 @@
-Shoes.app :title => "My Amazing Calculator", :width => 240, :height => 240 do
-  stack :margin => 20 do
+Shoes.app :title => "Simple Calculator", :width => 240, :height => 240 do
+  stack :margin => 30 do
     @output = edit_line
     
     flow do
-      %w(0 1 2 3 4 5 6 7 8 9 + / * -).each do |op|      
+      %w(1 2 3 * 4 5 6 / 7 8 9 + 0 %).each do |op|      
         button op do         
           append op
         end
       end
-      
+      button "C" do
+        @output.text = 0
+        @input = ""
+      end
       button "=" do
         eval_expression
       end
@@ -25,6 +28,7 @@ Shoes.app :title => "My Amazing Calculator", :width => 240, :height => 240 do
     @input += s
     @output.text = @input
   end
+
   
   # Evaluate the input we've got so far
   #
